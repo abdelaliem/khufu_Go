b6e9ac44d2ae4e149caf3025220abe7d972c453e
abdelaliem <abdelaliieem@gmail.com>
2024-02-09 22:19:45 +0200

    Delete placeApi.js

--- Backend/routes/api/placeApi.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const placeController = require('../../controller/placeController')
-const bus  = require('../../controller/busController')
-app.get('/places',placeController.getPlaces)
-app.get('/place/:lat/:lang', placeController.getPlaceId)
-app.post('/bus/:lat/:lang',bus.getbusNum)
-
