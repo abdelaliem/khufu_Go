6a4c15003588603d084667b77af8f92ee4ac7ee5
marbella267 <135547136+marbella267@users.noreply.github.com>
2024-02-09 21:42:27 +0200

    changes

--- Backend/controller/busController.js
+++ Backend/controller/busController.js
@@ -3,8 +3,7 @@ const getbusNum =  async (req,res)=>{
     try {
     const lat =  parseFloat(req.params.lat)
     const lang =  parseFloat(req.params.lang)
-    id = req.body['place_id']
-    console.log(id)
+    
     const data = await busModule.getBuses(lat,lang,id)
     res.send(data)
     } catch (error) {
