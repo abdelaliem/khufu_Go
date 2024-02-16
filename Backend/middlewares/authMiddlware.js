93e04fab308ac8dcdc602d719d073e6fef4feab6
Mohammed Sultan <111147918+SULTVNN@users.noreply.github.com>
2024-02-16 15:46:57 +0200

    authorization middleware to check user

---
+++ Backend/middlewares/authMiddlware.js
@@ -0,0 +1,19 @@
+const jwt =require('jsonwebtoken')
+
+const requireAuth = function (req, res, next) {
+    const token = req.body.jwt
+    if (token) {
+        jwt.verify(token , 'sultan is develober',(err , decodedToken)=>{
+            if (err) {
+                res.send("user not login yet")
+            }else{
+                next()
+            }
+        })
+    }else{
+        res.send("user not login yet")
+    }
+
+}
+
+module.exports= requireAuth;
