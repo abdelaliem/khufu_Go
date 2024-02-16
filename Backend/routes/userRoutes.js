0c9c8eb6a007dadf1f62c611e4df2ec2a047705a
Mohammed Sultan <111147918+SULTVNN@users.noreply.github.com>
2024-02-16 16:45:57 +0200

    Add files via upload

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const router = express.Router();
+const jwt =require('jsonwebtoken');
+const UserModel = require('../models/UserModel');
 
 router.post('/signup',userController.sginUp)
 router.post('/login',userController.login)
-
+router.get('/userinfo/:token',userController.userInfo)
 module.exports= router;
