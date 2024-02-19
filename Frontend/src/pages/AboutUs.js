e33e25d6e62f2851fdb5b22dad8813ea4eaafaaa
abdelaliem <abdelaliieem@gmail.com>
2024-02-19 23:20:16 +0200

    farida code

--- Frontend/src/pages/AboutUs.js
+++ Frontend/src/pages/AboutUs.js
@@ -1,26 +1,48 @@
 function AboutUs() {
-    return (
-        <div className="bg-gray-100 min-h-screen flex flex-col items-center">
-        <header className="w-full max-w-4xl mx-auto px-4 py-8">
-          <h1 className="text-4xl font-bold text-center">
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center bg-white">
+      {/* Left Column */}
+      <div className="w-full lg:w-1/2 p-7">
+        <header className="max-w-4xl mx-auto py-8">
+          <h1 className="text-2xl font-bold  indent-px">
             Your ultimate guide to hassle-free public transportation
           </h1>
         </header>
-  
-        <main className="w-full max-w-4xl mx-auto px-4 py-8 flex-1 text-center">
-          <p className="mb-8 text-xl">
-            Providing real-time information on public transportation services. Focusing on bus arrival times. User-friendly interface for effortless navigation and accessibility. Real-time bus arrival updates, route information, service alerts, and more. Feedback Mechanism: Option for users to provide input and report issues.
-          </p>
-  
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+
+        <p className="mb-8 text-lg leading-loose">
+          Providing real-time information on public transportation services.
+          <br />
+          <br />
+          Focusing on bus arrival times. <br />
+          <br /> User-friendly interface for effortless navigation and
+          accessibility.
+          <br />
+          <br />
+          Real-time bus arrival updates, route information, service alerts, and
+          more.
+          <br />
+          <br />
+          Feedback Mechanism: Option for users to provide input and report
+          issues.
+        </p>
+
+        <main className="max-w-4xl mx-auto text-center">
+          <button
+            href="#"
+            className="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+          >
             Get Started
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+          <button
+            href="#"
+            className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl ml-4"
+          >
             Sign Up
           </button>
         </main>
       </div>
-    )
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
