e33b3ee47d5c0f7b02e3251f308afbe3f77d89b9
Mohammed Sultan <111147918+SULTVNN@users.noreply.github.com>
2024-02-23 21:55:44 +0200

    clear the warnings from the console

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
-
+console.log = console.warn = console.error = () => {};
+console.error('Something bad happened.');
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
